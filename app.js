f80cfdad1f8935bde73d756c5adb696933f68bc4
Jenny <j.thurnau@hotmail.com>
2020-01-08 13:52:22 -0800

    jack and jenny magic

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
-
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+// const index = require('./index')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+})
